088a73071c183b1ab15dfe7719c814bc34226774
Jawad-Hassan-J <Jawad-Hassan-J@gmail.com>
2025-08-27 16:10:38 +0300

    sign-up, sign-in, log-out, home page, authnicatino

--- ai_assistant/core/models.py
+++ ai_assistant/core/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255) 
+
+    def __str__(self):
+        return self.username
 
 class Tool(models.Model):
     name = models.CharField(max_length=100) 
--- ai_assistant/core/views.py
+++ ai_assistant/core/views.py
@@ -1,9 +1,33 @@
 # https://simpleisbetterthancomplex.com/tutorial/2016/08/01/how-to-upload-files-with-django.html?utm_source
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin  
 from .router import execute
 
 def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+
+@login_required
+def plutonium(request):
     result = None
     user_input_request = request.POST.get("user_input_request", "")
     user_input_file = request.FILES.get("user_input_file")
@@ -11,8 +35,14 @@ def home(request):
     if request.method == "POST":
         result = execute(user_input_request, user_input_file)
 
-    return render(request, "home.html", {
+    return render(request, "plutonium.html", {
         "user_input_request": user_input_request,
         "user_input_file": user_input_file,
         "result": result
     })
+
+
+
+
+
+
